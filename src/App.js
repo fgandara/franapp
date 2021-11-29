f828b5f8a94d8273626a20c1c48be7ffac126451
Fran Gandara <fggandara@gmail.com>
2021-11-29 18:36:02 -0300

    changed something

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This app is awesome XD 
+          lets change something to see what happens
         </p>
         <a
           className="App-link"
